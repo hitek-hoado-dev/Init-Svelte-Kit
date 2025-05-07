7020c3256cf1e3244114637cd1b7168a6ddac0d1
HITEK SOFTWARE <hiteksoftware@13-2019-FVFZ74L8L410.local>
2025-05-07 17:25:38 +0700

    FIX: fix url api user detail local

--- src/routes/(private)/user/[id]/+page.server.ts
+++ src/routes/(private)/user/[id]/+page.server.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
+import { APP_API_URL } from "$env/static/private";
 
 export const csr = false;
 export const ssr = true;
 
 export const load = async ({ params }) => {
     const { id } = params;
-    const response = await axios.get(`http://localhost:5173/users/${id}`);
+    const response = await axios.get(`${APP_API_URL}/users/${id}`);
     return {
         user: response.data
     }
