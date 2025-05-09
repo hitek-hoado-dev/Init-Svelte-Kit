e56056477213a523f5033a720fcb144915ca2306
HITEK SOFTWARE <hiteksoftware@13-2019-FVFZ74L8L410.local>
2025-05-09 10:47:11 +0700

    FIX: remove middleware logic server side

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,25 +1,25 @@
-// src/routes/+layout.server.ts
-import type { LayoutServerLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
-import { STORAGES } from '$lib/constants/storage';
+// // src/routes/+layout.server.ts
+// import type { LayoutServerLoad } from './$types';
+// import { redirect } from '@sveltejs/kit';
+// import { STORAGES } from '$lib/constants/storage';
 
-export const load: LayoutServerLoad = async ({ cookies, url }) => {
-  console.log("root load server")
-  const token = cookies.get(STORAGES.ACCESS_TOKEN);
-  console.log(token);
-  const path  = url.pathname;
-  console.log(path);
-  const publicRoutes = ['/login','/signup'];
+// export const load: LayoutServerLoad = async ({ cookies, url }) => {
+//   console.log("root load server")
+//   const token = cookies.get(STORAGES.ACCESS_TOKEN);
+//   console.log(token);
+//   const path  = url.pathname;
+//   console.log(path);
+//   const publicRoutes = ['/login','/signup'];
   
-  if (!token && !publicRoutes.includes(path)) {
-    console.log('redirect to login')
-    throw redirect(303, '/login');
-  }
+//   if (!token && !publicRoutes.includes(path)) {
+//     console.log('redirect to login')
+//     throw redirect(303, '/login');
+//   }
 
-  if (token && publicRoutes.includes(path)) {
-    console.log('redirect to home')
-    throw redirect(303, '/');
-  }
+//   if (token && publicRoutes.includes(path)) {
+//     console.log('redirect to home')
+//     throw redirect(303, '/');
+//   }
 
-  return { token };
-};
+//   return { token };
+// };
